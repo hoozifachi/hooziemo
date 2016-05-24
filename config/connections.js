2473b12e7c715937e8f2c8e98687bebf02509a6f
Jason Sweet <hoozifachi@gmail.com>
2016-05-24 16:33:04 -0500

    Use local db

--- config/connections.js
+++ config/connections.js
@@ -56,13 +56,13 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMongodbServer: {
+  // localMongoDb: {
   //   adapter: 'sails-mongo',
   //   host: 'localhost',
   //   port: 27017,
-  //   user: 'username', //optional
-  //   password: 'password', //optional
-  //   database: 'your_mongo_db_name_here' //optional
+  //   // user: 'username', //optional
+  //   // password: 'password', //optional
+  //   // database: 'your_mongo_db_name_here' //optional
   // },
 
   /***************************************************************************
